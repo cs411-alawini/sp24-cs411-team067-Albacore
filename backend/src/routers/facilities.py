7668d99f813a093e6814a6c7ca0fcad0b58cc93e
LukeAlbarracin <lalbarracin2000@gmail.com>
2024-04-22 06:54:36 -0500

    Setup basic reverse proxy

--- backend/src/routers/facilities.py
+++ backend/src/routers/facilities.py
@@ -5,6 +5,7 @@ from ..db import db_instance
 from ..db.db_instance import get_cursor
 from fastapi.responses import JSONResponse
 from ..auth_bearer import JWTBearer
+import logging
 
 router = APIRouter()
 
@@ -19,6 +20,7 @@ class Facility(BaseModel):
 @router.get("/api/facilities", tags=["Facilities"])
 async def get_facilities():
     cursor = get_cursor()
+    logger.info("Facilities has been run")
     cursor.execute("SELECT * from Facilities")
     rows = cursor.fetchall()
     facilities = [Facility(location_id=row['LocationID'], bldg_name=row['BldgName'], floor_section=row['FloorSection'], longitude=row['Longitude'], latitude=row['Latitude'], map_url=row['MapURL']) for row in rows]
